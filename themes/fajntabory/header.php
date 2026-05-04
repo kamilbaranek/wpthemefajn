40f2243f28a4bc9ee4cb1d7b5ec832c45595286c
kamilbaranek <kamil.baranek@me.com>
2026-05-04 12:36:36 +0200

    Disable hardcoded tracking tags

--- themes/fajntabory/header.php
+++ themes/fajntabory/header.php
@@ -3,19 +3,21 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114677811-1"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
+	<?php if ( false ) : // Hardcoded Google tags disabled; managed via GTM plugin. ?>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114677811-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
 
-	  gtag('config', 'UA-114677811-1');
-	  gtag('config', 'AW-809603562');
-	</script>
-	<?php if(is_page_template( 'page-thankyou.php' ) && !empty($_GET['oid'])) : ?>
-	<!-- Event snippet for Nákup conversion page --> 
-	<script> gtag('event', 'conversion', { 'send_to': 'AW-809603562/3WcBCO6FmcQCEOqjhoID', '<?php echo $_GET['oid']; ?>': '' }); </script>
+		  gtag('config', 'UA-114677811-1');
+		  gtag('config', 'AW-809603562');
+		</script>
+		<?php if(is_page_template( 'page-thankyou.php' ) && !empty($_GET['oid'])) : ?>
+		<!-- Event snippet for Nákup conversion page -->
+		<script> gtag('event', 'conversion', { 'send_to': 'AW-809603562/3WcBCO6FmcQCEOqjhoID', '<?php echo $_GET['oid']; ?>': '' }); </script>
+		<?php endif; ?>
 	<?php endif; ?>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -61,17 +63,19 @@
 		}
 	?>
 	
-	<script>
-		(function(i,s,o,g,r,a,m){ i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		
-		ga('create', "UA-48737306-1", "fajntabory.cz");
-		ga('require', 'linkid', 'linkid.js');
-		ga('require', 'ecommerce', 'ecommerce.js');
-		ga('send', 'pageview');
-	</script>
+	<?php if ( false ) : // Hardcoded Universal Analytics disabled; managed via GTM plugin. ?>
+		<script>
+			(function(i,s,o,g,r,a,m){ i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', "UA-48737306-1", "fajntabory.cz");
+			ga('require', 'linkid', 'linkid.js');
+			ga('require', 'ecommerce', 'ecommerce.js');
+			ga('send', 'pageview');
+		</script>
+	<?php endif; ?>
 </head>
 
 <body <?php body_class(); ?>>
